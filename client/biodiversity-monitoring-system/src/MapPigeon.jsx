56c696b2f5957aa90a5cd78610b4ff08c6605eaf
LAB_F402 <labf402@continental.edu.pe>
2025-03-27 17:39:19 -0500

    Update MapPigeon.jsx

--- client/biodiversity-monitoring-system/src/MapPigeon.jsx
+++ client/biodiversity-monitoring-system/src/MapPigeon.jsx
@@ -2,23 +2,27 @@ import { Map, Marker } from "pigeon-maps";
 import { useEffect, useState } from "react";
 
 export function MyMap() {
-  const [data, setData] = useState(null)
-  const [apiData, setApiData] = useState(null)
+  const [data, setData] = useState(null);
+  const [apiData, setApiData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://apibioweb.com/api/Public/Amphibia/Species/Pristimantis%20achatinus")
+    fetch(`https://www.freetestapi.com/api/v1/animals?search=Lion`)
       .then(res => {
         if (!res.ok) {
           throw new Error("Error al cargar los datos");
         }
+        console.log(res.body);
         return res.json();
       })
       .then(response => {
-        setApiData(response)
+        setApiData(response);
+        setError(null); // Limpiar el error si la solicitud es exitosa
       })
       .catch(error => {
         console.error("Error:", error.message);
-        setApiData(null)
+        setError("No se pudieron cargar los datos. Inténtalo más tarde.");
+        setApiData(null);
       });
   }, []);
 
@@ -27,15 +31,13 @@ export function MyMap() {
       <Map
         height={300}
         defaultCenter={[-12.049041, -75.197788]}
-        defaultZoom={11}>
+        defaultZoom={17}>
         <Marker
           width={20}
           anchor={[-12.049041, -75.197788]}
           onClick={() => {
-            if (apiData) {
-              const formattedData = JSON.stringify(apiData, null, 2);
-              setData(formattedData);
-            }
+            setData(apiData[0].image)
+            console.log(apiData[0].image);
           }}
         />
         <Marker
@@ -43,12 +45,12 @@ export function MyMap() {
           anchor={[-12.049232, -75.198148]}
         />
       </Map>
+
       <div>
-        {data ? (
-          <pre>{data}</pre> // Mostrar el JSON formateado
-        ) : (
-          "Haz clic en el marcador para ver los datos"
-        )}
+        <h1>{animal}</h1>
+        <span>el león</span>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {data && <img src={data} alt="Animal" />}
       </div>
     </div>
   );
