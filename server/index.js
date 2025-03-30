a88e36bcb2e27c995f845ae5ef1146f05adb9f26
woshtsu <ricardoporrasveli@gmail.com>
2025-03-30 00:00:00 -0500

    Updated using Express and Erro-404

--- server/index.js
+++ server/index.js
@@ -1,50 +1,27 @@
-import http from 'node:http'
-import { createRequire } from 'node:module'
+import express from 'express'
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
 
-const require = createRequire(import.meta.url)
+const app = express()
 
-const animalsJSON = require('./src/fakeanimals.json')
+const PORT = process.env.PORT ?? 1234
 
-const desirePort = process.env.PORT ?? 1234
 
-const processRequest = (req, res) => {
-  const { method, url } = req
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Pagina Principalñ</h1>')
+})
 
-  if (url === '/') {
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-    return res.end(JSON.stringify(animalsJSON))
+app.use(async (req, res) => {
+  const filePath = join(process.cwd(), 'public', 'images', '404-error.svg')
+  try {
+    const content = await readFile(filePath, 'utf-8')
+    res.status(404).send(content)
+  } catch (error) {
+    console.error('Error al leer el svg', error)
+    res.status(500).send('Error interno del servidor')
   }
-  if (method === 'GET' && url === '/search') {
-    let animal
-    try {
-      animal = animalsJSON.find(animal => animal.id === 322)
-      if (!animal) {
-        throw new Error('salta al error')
-      }
-    } catch {
-      res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' })
-      res.end('<h1>No se encontro</h1>')
-    }
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
-    res.end(JSON.stringify(animal))
-  }
-  if (method === 'POST') {
-    if (url === '/create') {
-      let body = ''
-      req.on('data', chunk => {
-        body += chunk.toString()
-      })
-      req.on('end', () => {
-        const data = JSON.parse(body)
-        res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
-        res.end(JSON.stringify(data))
-      })
-    }
-  }
-}
+})
 
-const server = http.createServer(processRequest)
-
-server.listen(desirePort, () => {
-  console.log(`server listening on http://localhost:${desirePort}`)
+app.listen(PORT, () => {
+  console.log(`App listening on port http://localhost:${PORT}`)
 })
